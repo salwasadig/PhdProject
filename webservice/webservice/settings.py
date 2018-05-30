873f880cf015f1cd94144bc3d9beaa570215bcc1
salwasadig <salwa.sadig@hotmail.com>
2018-05-30 22:07:24 +0300

    main application init files

--- webservice/webservice/settings.py
+++ webservice/webservice/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'webservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phddb',
+        'USER': 'salwa',
+        'PASSWORD': 112358,
+        'HOST': 'localhost', 
+        'PORT': ''
     }
 }
 
